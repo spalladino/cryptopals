59cc4b6ee9402c0ce228aa1a036c2e395700e0a3
Santiago Palladino <spalladino@gmail.com>
2016-11-15 17:37:45 -0300

    Challenge 1.7
    Add AES ECB 128

---
+++ spec/set1/c7.cr
@@ -0,0 +1,23 @@
+require "../spec_helper"
+
+# AES in ECB mode
+# ---------------
+#
+# The Base64-encoded content in file c7.input.txt has been encrypted via AES-128 in ECB mode under the key
+# ```
+# "YELLOW SUBMARINE".
+# ```
+# (case-sensitive, without the quotes; exactly 16 characters; I like "YELLOW SUBMARINE" because it's exactly 16 bytes long, and now you do too).
+#
+# Decrypt it. You know the key, after all.
+#
+# Easiest way: use OpenSSL::Cipher and give it AES-128-ECB as the cipher.
+#
+describe "1.7" do
+  it "decrypts aes in ecb mode" do
+    input = Base64.decode(File.read("./spec/set1/data/c7.input.txt").strip).to_slice
+    key = "YELLOW SUBMARINE"
+    result = String.new(Cryptopals::AES.decrypt_ecb_128(key.to_slice, input))
+    result[0...33].should eq("I'm back and I'm ringin' the bell")
+  end
+end
---
+++ src/cryptopals/aes.cr
@@ -0,0 +1,21 @@
+require "openssl/cipher"
+
+
+
+module Cryptopals
+
+  module AES
+
+    def self.decrypt_ecb_128(key : Bytes, ciphertext : Bytes)
+      cipher = OpenSSL::Cipher.new("AES-128-ECB")
+      result = MemoryIO.new
+      cipher.decrypt
+      cipher.key = key
+      result.write(cipher.update(ciphertext))
+      result.write(cipher.final)
+      result.to_slice
+    end
+
+  end
+
+end
